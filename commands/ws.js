1c2d6a6a056c4c74aee9d8d8ea862434d7bac792
razorapple <vmrelaxed@yahoo.com>
2019-04-16 19:18:06 +0300

    fuck the police comming straight from the underground

--- commands/ws.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js")
-module.exports = {
-    name: "ws",
-    description: "websocket test",
-    usage: "[ws]",
-	cooldown: 5,
-	execute(client, message, args) {
-	
-  }}
